b4e2389c40eff0e2cf97deb30743cd1d3cfb3a8e
P3RCZIII <wiktor.perczak@gmail.com>
2023-07-20 13:09:54 +0200

    adding types and updating typescript

--- src/pages/category/clothing/index.tsx
+++ src/pages/category/clothing/index.tsx
@@ -1,38 +1,49 @@
-import Products from "@/components/Products/Products";
+import React from 'react';
+import Products from '@/components/Products/Products';
 
-export default function CategoryPage(props) {
-    return (
-      <div className="body">
-        <Products Items={props.products} />
-      </div>
-    );
-  }
-    
-  export async function getStaticProps() {
-      const response = await fetch(`https://fakestoreapi.com/products/category/men's%20clothing`);
-      const response2 = await fetch(`https://fakestoreapi.com/products/category/women's%20clothing`);
-      const menData = await response.json();      
-      const womenData = await response2.json();
-      
-      const products = [
-        ...menData.map((item) => ({
-          id: item.id,
-          title: item.title,
-          price: item.price,
-          category: item.category,
-          image: item.image,
-        })),
-        ...womenData.map((item) => ({
-          id: item.id,
-          title: item.title,
-          price: item.price,
-          category: item.category,
-          image: item.image,
-        })),
-      ];
-   return {
-        props: {
-          products,
-        },
-      };
-  }
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  image: string;
+}
+
+interface CategoryPageProps {
+  products: Product[];
+}
+
+export default function CategoryPage(props: CategoryPageProps): JSX.Element {
+  return (
+    <div className="body">
+      <Products Items={props.products} />
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const response = await fetch(`https://fakestoreapi.com/products/category/men's%20clothing`);
+  const response2 = await fetch(`https://fakestoreapi.com/products/category/women's%20clothing`);
+  const menData = await response.json();
+  const womenData = await response2.json();
+
+  const products: Product[] = [
+    ...menData.map((item: Product) => ({
+      id: item.id,
+      title: item.title,
+      price: item.price,
+      image: item.image,
+    })),
+    ...womenData.map((item: Product) => ({
+      id: item.id,
+      title: item.title,
+      price: item.price,
+      image: item.image,
+    })),
+  ];
+
+  return {
+    props: {
+      products,
+    },
+  };
+}
